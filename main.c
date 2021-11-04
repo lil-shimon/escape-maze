8ac003546b5f26e11922fd74ed6fdfc0e5a45454
shimon <69175188+lil-shimon@users.noreply.github.com>
2021-11-05 08:58:58 +0900

    refactor(comment): improve comment

--- main.c
+++ main.c
@@ -1,17 +1,46 @@
 #include <stdio.h>
 
-/// one block of maze
-enum MazeKind { PATH, WALL, START, END }; // kind of maze
-enum MazeFlag { VISITED, UNVISITED };     // flag of maze
+/**
+ * MazeKind
+ * PATH  = 迷路の道
+ * WALL  = 迷路の壁
+ * START = 迷路の始点
+ * END   = 迷路の終点
+ */
+enum MazeKind { PATH, WALL, START, END };
 
+/**
+ * MazeFlag
+ * 迷路の可視不可視
+ */
+enum MazeFlag { VISITED, UNVISITED };
+
+/**
+ * Maze
+ * 迷路の構造体
+ * MazeKind = 迷路の種類
+ * MazeFlag = 迷路の可視不可視
+ */
 typedef struct {
-    enum MazeKind kind; // kind of maze
-    enum MazeFlag flag; // flag of maze (visited or not)
+    enum MazeKind kind; 
+    enum MazeFlag flag; 
 } MazeCell;
 
-#define MAZE_WIDTH  5 // width of maze
-#define MAZE_HEIGHT 5 // height of maze
+/**
+ * 迷路の横幅
+ */
+#define MAZE_WIDTH  5
 
+/**
+ * 迷路の縦幅 
+ */
+#define MAZE_HEIGHT 5
+
+/**
+ * 迷路を表示する関数
+ * 
+ * @param maze 
+ */
 void displayMaze(MazeCell maze[MAZE_WIDTH][MAZE_HEIGHT]) {
   int i, j;
 
@@ -38,7 +67,7 @@ void displayMaze(MazeCell maze[MAZE_WIDTH][MAZE_HEIGHT]) {
 };
 
 int main(void) {
-  // maze
+  // 迷路のデータ
   MazeCell maze[MAZE_WIDTH][MAZE_HEIGHT] =
     {
       { {START, VISITED}, {PATH, UNVISITED}, {PATH, UNVISITED}, {PATH, UNVISITED}, {PATH, UNVISITED} },
@@ -48,6 +77,7 @@ int main(void) {
       { {WALL, UNVISITED}, {PATH, UNVISITED}, {PATH, UNVISITED}, {PATH, UNVISITED}, {END, VISITED} }
     };
 
+  // 迷路を表示
   displayMaze(maze);
 
   return 0;
